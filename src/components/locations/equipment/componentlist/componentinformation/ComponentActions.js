03ac554ef5805d6f2a165bb38c9a1ab99cb800b3
b17carma <b17carma@student.his.se>
2020-04-04 14:07:10 +0200

    Update ContainedOverlineText.js & ComponentActions.js text style

--- src/components/locations/equipment/componentlist/componentinformation/ComponentActions.js
+++ src/components/locations/equipment/componentlist/componentinformation/ComponentActions.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import {ListItem} from 'react-native-elements';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {StyleSheet} from 'react-native';
+import {material} from 'react-native-typography';
+
+const styles = StyleSheet.create({
+  titleText: {
+    ...material.body1,
+    fontSize: 15,
+  },
+});
 
 export default function ComponentActions(props) {
   return (
@@ -10,6 +19,7 @@ export default function ComponentActions(props) {
         <MaterialCommunityIcons name="scanner" color="#9E9E9E" size={26} />
       }
       title="Scan Now"
+      titleStyle={styles.titleText}
     />
   );
 }
--- src/components/util/ContainedOverlineText.js
+++ src/components/util/ContainedOverlineText.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Text} from 'react-native';
 import {StyleSheet} from 'react-native';
+import {material} from 'react-native-typography';
 
 const styles = StyleSheet.create({
   text: {
+    ...material.body1,
+    fontSize: 12,
+    letterSpacing: 1,
     marginTop: 10,
     marginLeft: 14,
     marginBottom: 6,
